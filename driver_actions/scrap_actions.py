12493c2b37295141850c226fc23eb8eb47fc81c6
Thierry Dev <83826994+thierryKeller-1@users.noreply.github.com>
2026-03-11 16:03:25 +0300

    Add files via upload

--- driver_actions/scrap_actions.py
+++ driver_actions/scrap_actions.py
@@ -12,17 +12,18 @@ def set_currency_to_eur(driver) -> None:
     try:
         driver.click_element_containing_text("XPF")
         time.sleep(1)
-        driver.click_element_containing_text("EUR")
+        driver.click_element_containing_text("XPF")
         time.sleep(1)
     except:
         pass
 
 def set_dates(driver, start_date:str, end_date:str) -> None:
+    print(f"setting dates {start_date} | {end_date}")
     try:
-        driver.scroll_into_view("input[id='datepicker-v-0-1-1-0-start']")
+        driver.scroll_into_view("input[id='datepicker-v-0-1-8-0-start']")
         time.sleep(1)
         driver.run_js(f"""
-            let date_start = document.getElementById('datepicker-v-0-1-1-0-start');
+            let date_start = document.getElementById('datepicker-v-0-1-8-0-start');
             date_start.value = "{start_date}";
 
             date_start.scrollIntoView();
@@ -30,7 +31,7 @@ def set_dates(driver, start_date:str, end_date:str) -> None:
             date_start.dispatchEvent(new Event('input', {{ bubbles : true }} ));
             date_start.dispatchEvent(new Event('change', {{ bubbles : true }} ));
 
-            let date_end = document.getElementById('datepicker-v-0-1-1-0-end');
+            let date_end = document.getElementById('datepicker-v-0-1-8-0-end');
             date_end.value = "{end_date}";
 
             date_end.dispatchEvent(new Event('input', {{ bubbles : true }} ));
